dff7f0e5370a5b759696735f9cafbbd495af78fe
jechol <ryanrhie9@gmail.com>
2017-02-09 17:11:12 +0900

    Supervisor module

--- mix.exs
+++ mix.exs
@@ -15,7 +15,8 @@ defmodule FooMod.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger],
+     mod: {FooMod.Application, []}]
   end
 
   # Dependencies can be Hex packages:
