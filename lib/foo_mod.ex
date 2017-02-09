76302a6ce384aa61f3537fe180e3cf5577fba356
jechol <ryanrhie9@gmail.com>
2017-02-09 17:10:11 +0900

    Non supervisor module

---
+++ lib/foo_mod.ex
@@ -0,0 +1,18 @@
+defmodule FooMod do
+  @moduledoc """
+  Documentation for FooMod.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FooMod.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/foo_mod_test.exs
@@ -0,0 +1,8 @@
+defmodule FooModTest do
+  use ExUnit.Case
+  doctest FooMod
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
